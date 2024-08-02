4679ad003cda43e4d8df36458162c87b738e020f
Tiago Fonseca <tiagoffonseca@gmail.com>
2024-08-02 17:21:09 +0100

    chore: tidy up example code

--- GenericMethodTests/Program.cs
+++ GenericMethodTests/Program.cs
@@ -1,22 +1,21 @@
-﻿// Get the MethodInfo for the generic method:
-using GenericMethods;
+﻿using GenericMethods;
 using System.Reflection;
 
 MethodInfo mi = typeof(Example).GetMethod("Generic")!;
-
-// Assign the desired type (e.g., int) to the type parameter:
+// Assign the desired type to the type parameter:
 MethodInfo miConstructed = mi.MakeGenericMethod(typeof(ExampleImplementation));
-
-
 object[] myArgs = [new ExampleImplementation { myValue = "1" }];
 // Invoke the constructed generic method:
 IEnumerable<IExample> ret = (IEnumerable<IExample>)miConstructed.Invoke(null, parameters: myArgs);
 
+Console.WriteLine($"Type {ret.GetType()}");
 
+// Assign a different type to the type parameter:
 miConstructed = mi.MakeGenericMethod(typeof(ExampleImplementation2));
-
 myArgs = [new ExampleImplementation2 { myValue = 100 }];
 // Invoke the constructed generic method:
 IEnumerable<IExample> ret2 = (IEnumerable<IExample>?)miConstructed.Invoke(null, parameters: myArgs);
+
+Console.WriteLine($"Type {ret2.GetType()}");
 
 Console.ReadLine();
