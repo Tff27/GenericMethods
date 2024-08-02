622aab31fe4fedde8e7ab9aede82d87d29bcfb70
Tiago Fonseca <tiagoffonseca@gmail.com>
2024-08-02 10:19:45 +0100

    Add project files.

---
+++ GenericMethodTests/Example.cs
@@ -0,0 +1,32 @@
+﻿namespace GenericMethods
+{
+    public interface IExample
+    {
+
+    }
+
+    public class ExampleImplementation : IExample
+    {
+        public required string myValue { get; set; }
+    }
+
+    public class ExampleImplementation2 : IExample
+    {
+        public required int myValue { get; set; }
+    }
+
+
+    public class Example
+    {
+        public static IEnumerable<T> Generic<T>(IExample toDisplay)
+            where T : IExample
+        {
+            var myList = new List<T>
+            {
+                (T)toDisplay,
+            };
+
+            return myList;
+        }
+    }
+}
---
+++ GenericMethodTests/Program.cs
@@ -0,0 +1,22 @@
+﻿// Get the MethodInfo for the generic method:
+using GenericMethods;
+using System.Reflection;
+
+MethodInfo mi = typeof(Example).GetMethod("Generic")!;
+
+// Assign the desired type (e.g., int) to the type parameter:
+MethodInfo miConstructed = mi.MakeGenericMethod(typeof(ExampleImplementation));
+
+
+object[] myArgs = [new ExampleImplementation { myValue = "1" }];
+// Invoke the constructed generic method:
+IEnumerable<IExample> ret = (IEnumerable<IExample>)miConstructed.Invoke(null, parameters: myArgs);
+
+
+miConstructed = mi.MakeGenericMethod(typeof(ExampleImplementation2));
+
+myArgs = [new ExampleImplementation2 { myValue = 100 }];
+// Invoke the constructed generic method:
+IEnumerable<IExample> ret2 = (IEnumerable<IExample>?)miConstructed.Invoke(null, parameters: myArgs);
+
+Console.ReadLine();
